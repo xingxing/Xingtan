e5c967be6d8332732a6910bc17025924d67acf08
Wade Xing <iamxingxing@gmail.com>
2011-07-05 15:33:19 +0800

    添加功能描述

---
+++ features/step_definitions/student_profile_steps.rb
@@ -0,0 +1,11 @@
+Given /^: 我已经登录我的名字是颜回$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^: 修改基本信息学号为(\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^: 我的基本信息中将会有学号为(\d+)$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
