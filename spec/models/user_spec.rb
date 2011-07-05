cc7bfd21c04225c2e80cbb5068930ef8daf612e9
Wade Xing <iamxingxing@gmail.com>
2011-07-05 09:37:01 +0800

    User 单元测试代码

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,26 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @valid_attributes = {
-      
-    }
+    @user = User.new(:name=>"颜回",:password=>"123456",:password_confirmation=>"123456")
   end
-
-  it "should create a new instance given valid attributes" do
-    User.create!(@valid_attributes)
+  
+  it "姓名和密码都符合要求应该通过验证" do
+    @user.should be_valid
+  end
+    
+  it "姓名为空将不能通过验证" do
+    @user.name = nil
+    @user.should_not be_valid
+  end
+  
+  it "密码为空不能通过验证" do
+    @user.password = nil
+    @user.password_confirmation = nil
+    @user.should_not be_valid
+  end
+  
+  it "密码和重复密码不一致不能通过验证" do
+    @user.password_confirmation = "000000"
+    @user.should_not be_valid
   end
 end
