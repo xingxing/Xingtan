e59e4cba4144064e63901d5cc90ff47fdf4d4721
Wade Xing <iamxingxing@gmail.com>
2011-07-08 15:00:03 +0800

    学生编辑基本资料

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ require 'digest/sha2'
 class User < ActiveRecord::Base
   validates_presence_of :name, :message => "请输入您的在册姓名"
   validates_uniqueness_of :name, :message => "已经有和您同名的同学注册了，请在您的名字后加入下划线+任意数字（如：颜回_1）"
-  validates_presence_of :password,:message => "密码不可以为空"
+  validates_presence_of :password,:on=>:create,:message => "密码不可以为空"
   
   validates_presence_of :student_id, :on=>:update,:message => "请输入您的学号"
   
