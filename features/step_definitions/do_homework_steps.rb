70bd6a8a3a486fb9b0641fab7482601096ffea8c
Wade Xing <iamxingxing@gmail.com>
2011-07-19 20:50:58 +0800

    添加 提交作业 功能描述文件和步骤定义文件

---
+++ features/step_definitions/do_homework_steps.rb
@@ -0,0 +1,11 @@
+Given /^: 我访问诗的发布作业页面$/ do
+  visit published_topics_path
+end
+
+When /^: 我选择 诗 这个课程$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^: 学而时习之 将出现载页面中$/ do
+  pending # express the regexp above with the code you wish you had
+end
